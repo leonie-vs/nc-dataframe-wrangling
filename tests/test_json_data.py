2827d27e03ede376bcce083ff1c6a10ba7c94dcf
Leonie von Schirach <leonie.schirach@web.de>
2025-11-10 15:25:39 +0000

    add test for increase_price to check it updates df with correctly increased price

--- tests/test_json_data.py
+++ tests/test_json_data.py
@@ -15,5 +15,5 @@ def test_increase_price_returns_same_df_when_passed_zero():
 # Test 3
 def test_increase_price_changes_price_by_given_percentage():
     df = create_df('doughnuts.json')
-    df2 = increase_price(df, 10)
-    assert df2['price'] == 10
+    df2 = increase_price(df, 50)
+    assert df2.loc[0, 'price'] == (df.loc[0, 'price'] * 1.50).round(2)
