9d64fbe51aa93f7b45ecba08e9c2934b06a71cb8
Leonie von Schirach <leonie.schirach@web.de>
2025-11-10 14:09:33 +0000

    add first test for create_df function

--- tests/test_json_data.py
+++ tests/test_json_data.py
@@ -1,2 +1,9 @@
 from src.json_data import create_df, increase_price, get_best_value
+import pandas as pd
 
+# Test 1
+def test_create_df_returns_dataframe_from_json_file():
+    df = create_df('doughnuts.json')
+    assert isinstance(df, pd.DataFrame)
+
+# Test 2
